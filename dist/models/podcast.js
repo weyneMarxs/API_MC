b1aa5ea8cf40d32420635748e9d7e33cdb9b6e99
weyne <weyneace@gmail.com>
2023-01-27 10:01:33 -0300

    Create podcast.js

---
+++ dist/models/podcast.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Podcast = void 0;
+const core_1 = require("./core");
+class Podcast extends core_1.Core {
+}
+exports.Podcast = Podcast;
