201ed11af66c1095e39830af77eca4e191047fec
weyne <weyneace@gmail.com>
2023-01-27 10:01:27 -0300

    Create galeria.js

---
+++ dist/models/galeria.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Galeria = void 0;
+const core_1 = require("./core");
+class Galeria extends core_1.Core {
+}
+exports.Galeria = Galeria;
