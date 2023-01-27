5cdcd0c89697787711c4522616d50f4271d10712
weyne <weyneace@gmail.com>
2023-01-27 10:02:31 -0300

    Create core.ts

---
+++ models/core.ts
@@ -0,0 +1,10 @@
+import { Document } from "mongoose";
+export abstract class Core extends Document{
+  titulo: String;
+  texto: String;
+  imagem: String;
+  dataPublicacao: Date;
+  tags: String;
+  link: String;
+  ativo: Boolean;
+}
