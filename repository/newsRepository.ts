a283cc41f29514c7f9251451e50dc1e5b86c1cf7
weyne <weyneace@gmail.com>
2023-01-27 10:02:54 -0300

    Create newsRepository.ts

---
+++ repository/newsRepository.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+mongoose.set("strictQuery", true);
+import { News } from "../models/news";
+
+const NewsSchema = new mongoose.Schema<News>({
+  titulo: {type: String},
+  chapeu: {type: String},
+  texto: {type: String},
+  autor: {type: String},
+  imagem: {type: String},
+  dataPublicacao: {type: Date},
+  tags: {type: String},
+  link: {type: String},
+  ativo: {type: Boolean}
+});
+
+export const NewsRepository = mongoose.model<News>("news", NewsSchema);
