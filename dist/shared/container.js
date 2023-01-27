e77caaf70ca1e0fb2e8fa74ccf567a097f43bc6e
weyne <weyneace@gmail.com>
2023-01-27 10:02:20 -0300

    Create container.js

---
+++ dist/shared/container.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const tsyringe_1 = require("tsyringe");
+const galeriaService_1 = require("../services/galeriaService");
+const newsService_1 = require("../services/newsService");
+const videosService_1 = require("../services/videosService");
+const podcastService_1 = require("../services/podcastService");
+tsyringe_1.container.register("INewsService", {
+    useClass: newsService_1.NewsService
+});
+tsyringe_1.container.register("IVideosService", {
+    useClass: videosService_1.VideosService
+});
+tsyringe_1.container.register("IGaleriaService", {
+    useClass: galeriaService_1.GaleriaService
+});
+tsyringe_1.container.register("IPodcastService", {
+    useClass: podcastService_1.PodcastService
+});
